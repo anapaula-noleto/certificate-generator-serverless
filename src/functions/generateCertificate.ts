1500894c5ae244ba427bd21746a623f551c3e3a6
Ana Paula Nolêto <apaulanazarenonoleto@gmail.com>
2022-09-21 11:11:48 -0300

    offline local dynamodb configured

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -20,7 +20,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
         id,
         name,
         grade,
-        created_at: new Date(),
+        created_at: new Date().getTime(),
       },
     })
     .promise();
